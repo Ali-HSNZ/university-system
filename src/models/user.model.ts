e69ceb738b89737928a7a3176abc66f44178ad65
Ali-HSNZ <Aliatraby@gmail.com>
2025-03-20 01:07:58 +0330

    fix: change user model (add national_id)

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -8,9 +8,10 @@ const UserModel = sequelizeConfig.define(
     {
         id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
         username: { type: DataTypes.STRING(50), allowNull: false, unique: true },
-        password_hash: { type: DataTypes.STRING(255), allowNull: false },
-        email: { type: DataTypes.STRING(100), allowNull: false, unique: true },
-        full_name: { type: DataTypes.STRING(100), allowNull: false },
+        password: { type: DataTypes.STRING(255), allowNull: false },
+        email: { type: DataTypes.STRING(100), allowNull: true, unique: true },
+        national_id: { type: DataTypes.STRING(10), allowNull: true, unique: true },
+        full_name: { type: DataTypes.STRING(100), allowNull: true },
         role: {
             type: DataTypes.ENUM('student', 'professor', 'education_assistant', 'university_president'),
             allowNull: false
