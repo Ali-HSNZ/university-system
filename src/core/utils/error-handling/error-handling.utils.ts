a9d5ae6198dfc99cdd1a1169151fb9de29b8ed24
Ali-HSNZ <Aliatraby@gmail.com>
2025-03-23 01:05:00 +0330

    feat: delete file in server when error happened

--- src/core/utils/error-handling/error-handling.utils.ts
+++ src/core/utils/error-handling/error-handling.utils.ts
@@ -1,20 +1,41 @@
 import { Response } from 'express'
 import { ValidationError } from 'yup'
 import httpStatus from 'http-status'
+import fs from 'fs'
 
-const errorHandling = (error: ValidationError | unknown, res: Response) => {
-    if (error instanceof ValidationError) {
-        const formattedErrors: Record<string, string> = {}
-        error.inner.forEach((err) => {
-            if (err.path) formattedErrors[err.path] = err.message
+const extractFilePaths = (error: unknown): string[] => {
+    if (!error || typeof error !== 'object') return []
+
+    return Object.values(error)
+        .flatMap((value) => (typeof value === 'object' && value !== null ? Object.values(value) : []))
+        .filter((file) => file && typeof file === 'object' && 'path' in file && typeof file.path === 'string')
+        .map((file: any) => file.path)
+}
+
+const errorHandling = (error: unknown, res: Response) => {
+    const fileExists: string[] = extractFilePaths(error)
+    if (fileExists.length) {
+        fileExists.forEach((filePath) => {
+            if (fs.existsSync(filePath)) fs.unlinkSync(filePath)
         })
-        res.status(httpStatus.BAD_REQUEST).json({
-            status: 'error',
+    }
+
+    if (error instanceof ValidationError) {
+        const formattedErrors = Object.fromEntries(
+            error.inner.map((err) => [err.path, err.message]).filter(([path]) => path)
+        )
+
+        return res.status(422).json({
+            status: 422,
             message: 'اطلاعات وارد شده معتبر نیست',
             errors: formattedErrors
         })
     }
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: 'خطایی نامشخص رخ داده است' })
+
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+        status: httpStatus.INTERNAL_SERVER_ERROR,
+        message: error instanceof Error ? error.message : 'خطایی نامشخص رخ داده است'
+    })
 }
 
 export default errorHandling
