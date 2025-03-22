0a6d35e9125d0e0594237921d7a6145fd8c61edd
Ali-HSNZ <Aliatraby@gmail.com>
2025-03-23 01:10:41 +0330

    fix: update user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -7,16 +7,40 @@ const UserModel = sequelizeConfig.define(
     'users',
     {
         id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-        username: { type: DataTypes.STRING(50), allowNull: false, unique: true },
+        first_name: { type: DataTypes.STRING(100), allowNull: true },
+        last_name: { type: DataTypes.STRING(100), allowNull: true },
+        gender: {
+            type: DataTypes.ENUM('male', 'female'),
+            allowNull: false,
+            defaultValue: 'male'
+        },
+        avatar: { type: DataTypes.STRING(255), allowNull: true },
+        student_code: { type: DataTypes.STRING(10), allowNull: true },
+        phone: { type: DataTypes.STRING(11), allowNull: true },
+        professor_code: { type: DataTypes.STRING(10), allowNull: true },
+        training_course_code: { type: DataTypes.ENUM('11', '22'), allowNull: true },
+        deputy_code: { type: DataTypes.STRING(10), allowNull: true },
         password: { type: DataTypes.STRING(255), allowNull: false },
-        email: { type: DataTypes.STRING(100), allowNull: true, unique: true },
-        national_id: { type: DataTypes.STRING(10), allowNull: true, unique: true },
-        full_name: { type: DataTypes.STRING(100), allowNull: true },
+        national_code_image: { type: DataTypes.STRING(255), allowNull: true },
+        military_image: { type: DataTypes.STRING(255), allowNull: true },
+        military_status: {
+            type: DataTypes.ENUM('active', 'completed', 'exempted', 'postponed'),
+            allowNull: true,
+            defaultValue: 'active'
+        },
+        student_status: {
+            type: DataTypes.ENUM('active', 'deActive', 'studying', 'active', 'graduate'),
+            allowNull: true,
+            defaultValue: 'active'
+        },
+        email: { type: DataTypes.STRING(100), allowNull: true },
+        national_code: { type: DataTypes.STRING(10), allowNull: true },
         role: {
             type: DataTypes.ENUM('student', 'professor', 'education_assistant', 'university_president'),
             allowNull: false
         },
-        entry_year: { type: DataTypes.STRING, allowNull: true }
+        entry_date: { type: DataTypes.STRING, allowNull: true },
+        entry_semester: { type: DataTypes.ENUM('1', '2'), allowNull: true }
     },
     { timestamps: true, freezeTableName: true }
 )
