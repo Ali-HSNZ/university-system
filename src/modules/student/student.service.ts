75732688ae34e6300c458c75aad4fe04c7511642
Ali-HSNZ <Aliatraby@gmail.com>
2025-04-01 16:06:27 +0330

    fix: change student list attributes

--- src/modules/student/student.service.ts
+++ src/modules/student/student.service.ts
@@ -1,3 +1,4 @@
+import { APP_ENV } from '../../core/config/dotenv.config'
 import { DegreeModel } from '../../models/degree.model'
 import { DepartmentModel } from '../../models/department.model'
 import { HighSchoolDiplomaModel } from '../../models/highSchoolDiploma.model'
@@ -8,11 +9,45 @@ const studentService = {
     list: async () => {
         const students = await StudentModel.findAll({
             include: [
-                { model: UserModel },
-                { model: DegreeModel },
-                { model: DepartmentModel },
-                { model: HighSchoolDiplomaModel }
-            ]
+                {
+                    model: UserModel,
+                    attributes: {
+                        exclude: [
+                            'id',
+                            'createdAt',
+                            'updatedAt',
+                            'password',
+                            'is_deleted',
+                            'deleted_by',
+                            'deleted_at',
+                            'updated_at'
+                        ]
+                    }
+                },
+                { model: DegreeModel, attributes: { exclude: ['id', 'createdAt', 'updatedAt'] } },
+                { model: DepartmentModel, attributes: { exclude: ['id', 'createdAt', 'updatedAt'] } },
+                {
+                    model: HighSchoolDiplomaModel,
+                    attributes: { exclude: ['id', 'createdAt', 'updatedAt', 'user_id', 'pre_degree_id'] },
+                    include: [{ model: DegreeModel, attributes: { exclude: ['id', 'createdAt', 'updatedAt'] } }]
+                }
+            ],
+            attributes: {
+                exclude: ['updatedAt', 'pre_degree_id', 'department_id', 'high_school_diploma_id', 'user_id']
+            }
+        })
+
+        const protocol = APP_ENV.application.protocol
+        const host = APP_ENV.application.host
+        const port = APP_ENV.application.port
+
+        const BASE_URL = `${protocol}://${host}:${port}`
+
+        return students.map((student) => {
+            if (student.dataValues.user && student.dataValues.user.avatar) {
+                student.dataValues.user.avatar = `${BASE_URL}${student.dataValues.user.avatar}`
+            }
+            return student
         })
         return students
     }
